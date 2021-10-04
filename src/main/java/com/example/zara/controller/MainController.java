8d4d83f480e8d13638c448a4ac73de1e9930b67d
park517 <park51777@naver.com>
2021-10-04 15:03:57 +0900

    부트스트랩 템플릿 적용

--- src/main/java/com/example/zara/controller/MainController.java
+++ src/main/java/com/example/zara/controller/MainController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class MainController {
 
+@Controller
+public class MainController {
+	@GetMapping("/")
+	public String showMain() {
+		return "index";
+	}
 }
