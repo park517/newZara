1beabeaf95fe699c2d2b5e478837421b47f81f50
park517 <park51777@naver.com>
2021-10-04 12:04:33 +0900

    프로젝트 초기 셋팅

---
+++ src/main/java/com/example/zara/controller/MainController.java
@@ -0,0 +1,9 @@
+package com.example.zara.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class MainController {
+
+}
